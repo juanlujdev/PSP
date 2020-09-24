da5b198395a544053ddc57a948418a3293530867
juan_lujan <juanilloxyz@hotmail.com>
2020-09-24 20:23:11 +0200

    ejercicio arrays 2

--- AE1/src/es/florida/App.java
+++ AE1/src/es/florida/App.java
@@ -1,7 +1,32 @@
 package es.florida;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public void sayHellow(){
         System.out.println("Hola Mundo");
     }
+
+    public static void arrayNombre(){
+        String[] alumno = {"Javier", "Alfredo","Gilberto","Juan Antonio","Fabian","Ruben"};
+
+        List<String> alumnosLista = new ArrayList<>();
+        alumnosLista.add("juan");
+        alumnosLista.add("Sonia");
+        alumnosLista.add("Pedro");
+        alumnosLista.add("Paco");
+        alumnosLista.add("Angel");
+
+        for (int i=0;i<alumno.length;i++){
+            System.out.println(alumno[i]);
+        }
+
+       for (String i: alumnosLista)
+           System.out.println(i);
+
+
+    }
+
+
 }
